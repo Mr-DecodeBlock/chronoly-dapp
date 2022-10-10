8f11f08e03e8a466782175f24a762d9cc96899cf
abdec2 <abdec2@hotmail.com>
2022-10-10 16:04:27 +0300

    deploy to mainnet

--- src/abi/config.js
+++ src/abi/config.js
@@ -1,12 +1,12 @@
 
 const CONFIG = {
-    chainId: 5,
+    chainId: 1,
     // contractAddress: "0xa6CbC98Bb8AaAb6A54Ff2EF915152e4ba6d8D551", 
-    contractAddress: "0xa6CbC98Bb8AaAb6A54Ff2EF915152e4ba6d8D551",
-    tokenAddress: "0x2114Cbc56Ac2B8205772aAb5a2414fBF51362C82", 
+    contractAddress: "0x5449ab7bC1AAede61D380F0Ee979C9B9905f64E4",
+    tokenAddress: "0xE23311294467654E0CaB14cD32A169A41be5ca8E", 
     tokenDecimals: '18',
     tokenSymbol: 'CRNO', 
-    networkName: 'Goerli Testnet'
+    networkName: 'Ethereum Mainnet'
 }
 
 export default CONFIG;
